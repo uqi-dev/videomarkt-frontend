26b7fc37002f0b2299746f4f8434f640809b448c
Islam <MdZahidul.Islam@cbre.com>
2024-09-05 11:56:18 +0200

    improve design

--- src/app/media-details/[id]/page.tsx
+++ src/app/media-details/[id]/page.tsx
@@ -10,45 +10,48 @@ import Header from "@/app/header/header";
 const MediaDetails: React.FC = () => {
     const router = useRouter();
     const { id } = useParams();
-    const [service, setService] = useState<any>(null);
+    const [media, setMedia] = useState<any>(null);
 
     useEffect(() => {
         if (id) {
             fetch(`http://localhost:3001/media/${id}`)
                 .then((res) => res.json())
-                .then((data) => setService(data))
+                .then((data) => setMedia(data))
                 .catch((err) => console.error(err));
         }
     }, [id]);
 
-    if (!service) {
+    console.log(media);
+    if (!media) {
         return <p>Loading...</p>;
     }
 
     return (
-        <div className="service-details-container">
+        <div className="media-details-container">
             <div>
                 <Header />
             </div>
-            <div className="service-details-content">
-                <div className="service-image">
-                    <img src={service.thumbnail} alt={service.title}/>
+            <div className="media-details-content">
+                <div className="media-image">
+                    <img src={media.thumbnail} alt={media.title}/>
                 </div>
 
-                <div className="service-info">
-                    <h1>{service.title}</h1>
-                    <p>{service.description}</p>
-                    <div className="service-attributes">
-                        <p><strong>Price:</strong> ${service.price}</p>
-                        <p><strong>Resolution:</strong> {service.resolution}</p>
-                        <p><strong>Length:</strong> {service.length}</p>
-                        <p><strong>File Size:</strong> {service.fileSize}</p>
-                        <button className="buy-button">Buy for ${service.price}</button>
+                <div className="media-info">
+                    <h1>{media.title}</h1>
+                    <p>{media.description}</p>
+                    <div className="media-attributes">
+                        <p><strong>Organization name:</strong> {media.organization.name}</p>
+                        <p><strong>Author:</strong> {media.organization.users[0].firstName}</p>
+                        <p><strong>Price:</strong> ${media.price}</p>
+                        <p><strong>Resolution:</strong> {media.resolution}</p>
+                        <p><strong>Length:</strong> {media.length}</p>
+                        <p><strong>File Size:</strong> {media.fileSize}</p>
+                        <button className="buy-button">Buy for ${media.price}</button>
                     </div>
                 </div>
             </div>
 
-            <div className="related-services">
+            <div className="related-media">
                 <h3>You may like more options</h3>
                 <ServiceGrid/>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "@/app/hero/hero";
 import Header from "@/app/header/header";
 import ServiceGrid from "@/app/service-grid/service-grid";
+import './app.css'
 
 export default function Home() {
   return (
-      <div>
+      <div className="main-container">
         <Header/>
         <Hero/>
         <ServiceGrid/>
